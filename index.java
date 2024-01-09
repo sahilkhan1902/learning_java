040265ceee5a0620883b9d3f4986e2ed0dd8c892
sahil khan <sahilkhan2002786@gmail.com>
2024-01-09 21:52:22 +0530

    type casting adn conversion learn

--- index.java
+++ index.java
@@ -1,15 +1,17 @@
 import java.util.*;
-public class Index{
-      public static void main(String  []args) {
-       Scanner sc = new Scanner(System.in);
-     
-       float radius = sc.nextFloat();
-       float area = 3.14f * radius * radius;
-       System.out.print(area);
-        
-    }
 
-}
+// calculating the area of a circle
+// public class Index{
+//       public static void main(String  []args) {
+//        Scanner sc = new Scanner(System.in);
+     
+//        float radius = sc.nextFloat();
+//        float area = 3.14f * radius * radius;
+//        System.out.print(area);
+        
+//     }
+
+// }
     //    int a = sc.nextInt();
     //    int b = sc.nextInt();
     //    int sum = a * b;
@@ -23,3 +25,46 @@ public class Index{
     //  System.out.println("the value of a is " +  a);
     //  String name = "Tony Stark";
     //  System.out.println("the name is " +  name);
+
+
+    // type casting in java
+// public class Index{
+//       public static void main(String  []args) {
+//        Scanner sc = new Scanner(System.in);
+     
+//        float number = 25.23f;
+//     //    int a = number;   -- in this it will throw error
+//        int a = (int) number; // in this case it will work
+//        System.out.print(a);
+        
+//     }
+
+// }
+
+
+// using character to integer
+// public class Index{
+//       public static void main(String  []args) {
+//        char a = 's';
+//          int b = a; // in this case it will work
+//          //   char conversion to number work because every char hold some associate value 
+//        System.out.print(b);
+        
+//     }
+
+// }
+
+// Type conversion also knows as widening/implicit type
+/* conversion happens when 
+- type compatible
+- destination type > source type
+byte -> short -> int -> float -> long -> short*/
+public class Index{
+      public static void main(String  []args) {
+       int a = 25;
+         long b = a; 
+        System.out.print(b);
+        
+    }
+
+}
