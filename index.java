ad395d2460ff2c2d52754fffb8ce7ae13035fc25
sahil khan <sahilkhan2002786@gmail.com>
2024-01-09 21:26:59 +0530

    first commit

---
+++ index.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Index{
+      public static void main(String  []args) {
+       Scanner sc = new Scanner(System.in);
+     
+       float radius = sc.nextFloat();
+       float area = 3.14f * radius * radius;
+       System.out.print(area);
+        
+    }
+
+}
+    //    int a = sc.nextInt();
+    //    int b = sc.nextInt();
+    //    int sum = a * b;
+    //    System.out.print(sum);
+        // Scanner sc = new Scanner(System.in);
+        // String name = sc.nextLine();
+        // System.out.print(name);
+
+
+    //  int a =5;
+    //  System.out.println("the value of a is " +  a);
+    //  String name = "Tony Stark";
+    //  System.out.println("the name is " +  name);
