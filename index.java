902cdb46063b6938f821c55d0a98cc6139c3b1fc
sahil khan <sahilkhan2002786@gmail.com>
2024-01-09 22:20:46 +0530

    Type promotion learn

--- index.java
+++ index.java
@@ -27,19 +27,7 @@ import java.util.*;
     //  System.out.println("the name is " +  name);
 
 
-    // type casting in java
-// public class Index{
-//       public static void main(String  []args) {
-//        Scanner sc = new Scanner(System.in);
-     
-//        float number = 25.23f;
-//     //    int a = number;   -- in this it will throw error
-//        int a = (int) number; // in this case it will work
-//        System.out.print(a);
-        
-//     }
 
-// }
 
 
 // using character to integer
@@ -54,17 +42,68 @@ import java.util.*;
 
 // }
 
+    // type casting in java
+// public class Index{
+//       public static void main(String  []args) {
+//        Scanner sc = new Scanner(System.in);
+     
+//        float number = 25.23f;
+//     //    int a = number;   -- in this it will throw error
+//        int a = (int) number; // in this case it will work
+//        System.out.print(a);
+        
+//     }
+
+// }
+
 // Type conversion also knows as widening/implicit type
 /* conversion happens when 
 - type compatible
 - destination type > source type
 byte -> short -> int -> float -> long -> short*/
+// public class Index{
+//       public static void main(String  []args) {
+//        int a = 25;
+//          long b = a; 
+//         System.out.print(b);
+        
+//     }
+
+// }
+
+// Type promotion in Java
+// if there is an expression java automatically promotes byte, short or long to int value
+// if one expression is long, float or double the whole expression is promotes to long, float or long respectively
 public class Index{
       public static void main(String  []args) {
-       int a = 25;
-         long b = a; 
-        System.out.print(b);
+
+    // ---- for first condition
+    //    char a = 'a';
+    //      char b ='b'; 
+        //  char c = a-b; -- it will not work as c is char type and after adding in expression a-b become interger type
+        // System.out.println(b-a);
+
+        // short a = 25;
+        // byte b = 2;
+        // char c = 'c';
+        // byte = a+b+c; //if will throw an error because after express it become into the int
         
+
+        //---- for second case----
+        // int a =10;  // in this scenario max data type is double so all value will be convert to double
+        // float b =20-23f;
+        // long c = 26;
+        // double d = 40;
+        // double e = a+b+c+d;
+        // System.out.print(e);
+
+
+        byte b = 10;
+        // byte c = b*2;
+         byte c = (byte) (b*2);  // where as  -- in this case work because explicitly we redeclared the data typw
+        System.out.print(c); // here it will throw an error because after expr. it will become an int
+       
+       
     }
 
 }
