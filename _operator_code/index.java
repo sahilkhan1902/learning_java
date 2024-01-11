b172a9bc3bdc0d0b43dbddaa2abba84d6581de77
sahil khan <sahilkhan2002786@gmail.com>
2024-01-11 22:12:41 +0530

    operator code

---
+++ _operator_code/index.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+//  calculating the area of a circle
+ public class index{
+       public static void main(String  []args) {
+        Scanner sc = new Scanner(System.in);
+     
+        float radius = sc.nextFloat();
+        float area = 3.14f * radius * radius;
+        System.out.print(area);
+        
+     }
+
+ }
