3009108202c986d46b27257914cf12078955f379
sahil khan <sahilkhan2002786@gmail.com>
2024-01-12 23:06:34 +0530

    Till logical operator

--- _operator_code/index.java
+++ _operator_code/index.java
@@ -1,14 +1,58 @@
 import java.util.*;
 
+//Operators in Java
+
+
+/*types of operator
+-Arithmetic Operator
+    Binary (+ - / * %) and Unary ( ++, --)
+-Realational Operator
+-Logical Operator
+-Bitwise Operator
+-Assignment Operator
+*/
 //  calculating the area of a circle
  public class index{
-       public static void main(String  []args) {
-        Scanner sc = new Scanner(System.in);
+      public static void main(String  []args) {
+
+    // Arithmetic Operator
+     int num1 =50;
+     int num2 =625;
+     System.out.println("Add = " + (num2+num1));
+     System.out.println("Subtract = " + (num2-num1));
+     System.out.println("Multiply = " + (num2*num1));
+     System.out.println("Divide = " + (num2/num1));
+
+     /*Unary Operator
+     pre or post (inremet or decrement)
+     ++a pre increment a++ post increment or vice versa for decrement
+      */
+      System.out.println("Unary pre Increment = " + (++num1));
+      System.out.println("Unary post Increment = " + (num1++));
+      System.out.println("Unary pre Decrement = " + (--num1));
+      System.out.println("Unary post Decrement = " + (num1--));
      
-        float radius = sc.nextFloat();
-        float area = 3.14f * radius * radius;
-        System.out.print(area);
-        
+     /*Relational Operator
+   == equal
+   != not equal
+   > Less
+   < Great
+   <= 
+   >=
+      */
+
+        System.out.println("Relational operation using equal = " + (num1 == num2));
+        System.out.println("Relational operation using not euqal = " + (num1 != num2));
+        System.out.println("Relational operation using Less = " + (num1 > num2));
+        System.out.println("Relational operation using greater " + (num1 < num2));
+        System.out.println("Relational operation using less than equal to =  " + (num1 >= num2));
+        System.out.println("Relational operation using Great than equal to = " + (num1 <= num2));
+
+          /*Logical Operator
+  && (Logical And)
+  || (Logical OR)
+  !  ((Logical Not))
+      */
      }
 
  }
