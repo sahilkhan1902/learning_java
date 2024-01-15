aa699281954ec7dfb80f21d59b5d2baeb972672a
sahil khan <sahilkhan2002786@gmail.com>
2024-01-15 21:55:37 +0530

    condtional code

--- _conditional_statement/index.java
+++ _conditional_statement/index.java
@@ -24,12 +24,30 @@ public class index{
 
 
         // ----- it will check wether no is even or odd
-         int A = sc.nextInt();
-         if(A%2==0){
-            System.out.println("A is even with value = " +A); //it will check wthe conditon and return the print string
+        //  int A = sc.nextInt();
+        //  if(A%2==0){
+        //     System.out.println("A is even with value = " +A); //it will check wthe conditon and return the print string
+        // }
+        // else{
+        //     System.out.println("A is odd with value = " +A) ;
+        // }
+
+         // ----- it will check income tax on given income
+        int income = sc.nextInt();
+       
+         if(income<500000){
+            System.out.println("0% tax will be applicable on your income = " +income); //it will check wthe conditon and return the print string
+         }
+        else if(income>500000 && income<=1000000){
+            System.out.println("20%  tax will be applicable on your income = " +income) ;
+            System.out.println("And your tax will be = " +income*0.2 + "Rs");
         }
         else{
-            System.out.println("A is odd with value = " +A) ;
+             System.out.println("30%  tax will be applicable on your income = " +income) ;
+               System.out.println("And your tax will be = " +income*0.3 + "Rs");
         }
+        
+
+
     }
 }
