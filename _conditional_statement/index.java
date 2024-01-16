3534d8a99f3442663ec3226d1eebc9cfb3b1c3a4
sahil khan <sahilkhan2002786@gmail.com>
2024-01-16 21:45:06 +0530

    Conditional codex

--- _conditional_statement/index.java
+++ _conditional_statement/index.java
@@ -32,21 +32,70 @@ public class index{
         //     System.out.println("A is odd with value = " +A) ;
         // }
 
-         // ----- it will check income tax on given income
-        int income = sc.nextInt();
+         // ----- it will check income tax on given income by taking input from the user
+        // int income = sc.nextInt();
        
-         if(income<500000){
-            System.out.println("0% tax will be applicable on your income = " +income); //it will check wthe conditon and return the print string
-         }
-        else if(income>500000 && income<=1000000){
-            System.out.println("20%  tax will be applicable on your income = " +income) ;
-            System.out.println("And your tax will be = " +income*0.2 + "Rs");
+        //  if(income<500000){
+        //     System.out.println("0% tax will be applicable on your income = " +income); //it will check wthe conditon and return the print string
+        //  }
+        // else if(income>500000 && income<=1000000){
+        //     System.out.println("20%  tax will be applicable on your income = " +income) ;
+        //     System.out.println("And your tax will be = " +income*0.2 + "Rs");
+        // }
+        // else{
+        //      System.out.println("30%  tax will be applicable on your income = " +income) ;
+        //        System.out.println("And your tax will be = " +income*0.3 + "Rs");
+        // }
+        
+
+        // ---- it will check the which no among a, b and c are greater
+       /* int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        if(A>B && A>C){
+            System.out.println(" A is greates with value = " + A);
+        }
+        else if(B>C){
+            System.out.println(" B is greates with value = " + B);
         }
         else{
-             System.out.println("30%  tax will be applicable on your income = " +income) ;
-               System.out.println("And your tax will be = " +income*0.3 + "Rs");
-        }
-        
+            System.out.println(" C is greates with value = " + C);
+        }*/
+
+        //--- terniary operator
+    //     int A =sc.nextInt();
+    //   System.out.println(A>10 ?"red": "blue");
+
+    //-- using switch statement
+    // int number =2;
+    // switch(number){
+    //     case 1 : System.out.println("aam");
+    //       break;
+    //     case 2 : System.out.println("naam");
+    //       break;
+    //     case 3 : System.out.println("kaam");
+    //       break;
+    // }
+
+    // --- making calculator
+    int num1 = sc.nextInt();
+    System.out.println("Entered num1 = " + num1);
+    int num2 = sc.nextInt();
+    System.out.println("Entered num1 = " + num2);
+    char operate = sc.next().charAt(0);
+      System.out.println("choose operator is = " + operate);
+
+      switch(operate){
+        case '+' : System.out.println("the sum of num1 and num2 = "+ (num1 + num2));
+        break;
+        case '-' : System.out.println("the sub of num1 and num2 = "+ (num1 - num2));
+        break;
+        case '*' : System.out.println("the multiple of num1 and num2 = "+ (num1 * num2));
+        break;
+        case '/' : System.out.println("the division of num1 and num2 = "+ (num1 / num2));
+        break;
+      }
+
 
 
     }
