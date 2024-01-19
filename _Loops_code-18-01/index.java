86bda040d64d26817610b71d24006e36b9594396
sahil khan <sahilkhan2002786@gmail.com>
2024-01-19 22:42:49 +0530

    pattern code

--- _Loops_code-18-01/index.java
+++ _Loops_code-18-01/index.java
@@ -9,8 +9,10 @@ For Loops
    do something }*/
 
 // While Loopsp
+import java.util.*;
 public class index{
     public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
         // int i =0; // initialization
         // while( i<11 ){  // check the condition here
         //       System.out.println(i); // execution 
@@ -20,14 +22,38 @@ public class index{
         // for(int i=1; i<5;i++){// (initIialization, condition, updation)
         //        System.out.println(i);
         // }
-      int n;
-      int sum =0;
-      int i=1;
-      while(i<=5){
-        sum=sum+i;
-        i++;
+    //   int n =sc.nextInt();
+    //   int sum =0;
+    //   int i=1;
+    //   while(i<=n){
+    //     sum=sum+i;
+    //     i++;
 
-      }
-        System.out.println("sum is " +sum);
-    }
+    //   }
+    //     System.out.println("sum is " +sum);
+
+
+// 4*4 star pattern using for loopz
+    // for(int i=1; i<=4; i++){
+    //     System.out.println("****");
+    // }
+    // }
+
+
+// star pattern using while loop
+    // int i =1;
+// while(i<=4){
+//     System.out.println("******");
+//     i++;
+// };
+
+
+int n =sc.nextInt();
+while(n>0){
+    int reversedigit =n%10;
+    System.out.print(reversedigit);
+    n/=10;
 }
+System.out.println();
+    }
+};
