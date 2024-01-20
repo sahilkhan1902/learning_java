477923f21817c41702af55aee527a4f10f1c5815
sahil khan <sahilkhan2002786@gmail.com>
2024-01-21 00:06:47 +0530

    do while loop

--- _Loops_code-18-01/index.java
+++ _Loops_code-18-01/index.java
@@ -47,16 +47,20 @@ public class index{
 //     i++;
 // };
 
-
-int n =sc.nextInt();
-int rev=0;
-while(n>0){
-    int lastdigit =n%10;
-    rev=(rev*10)  + lastdigit;
-    System.out.print(lastdigit);
-    n/=10;
-}
-System.out.println("rev");
-// System.out.println(rev);
+// reverse a no using while loop
+// int n =sc.nextInt();
+// int rev=0;
+// while(n>0){
+//     int lastdigit =n%10;
+//     rev=(rev*10)  + lastdigit;
+//     System.out.print(lastdigit);
+//     n/=10;
+// }
+// System.out.println("rev");
+int n=1;
+do{
+    System.out.println("hello world");
+    n++;
+}while(n<=10);
     }
 };
