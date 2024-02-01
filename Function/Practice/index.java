f46c3b012ea99c875afa4a97ce6231a971d6f57b
sahil khan <sahilkhan2002786@gmail.com>
2024-02-01 22:54:07 +0530

    function basic

---
+++ Function/Practice/index.java
@@ -0,0 +1,14 @@
+package Function.Practice;
+import java.util.*;
+public class index {
+    public static void printMyName(String name) {
+        System.out.print(name);
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        printMyName(name);
+    }
+    
+}
