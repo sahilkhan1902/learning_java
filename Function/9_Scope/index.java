fd9781119922af3dbb9ebef0fd79dacf75f8eac9
sahil khan <sahilkhan2002786@gmail.com>
2024-02-15 23:06:56 +0530

    Scope in java

---
+++ Function/9_Scope/index.java
@@ -0,0 +1,17 @@
+/*
+ * Scope 
+   - Block scope:- The initialize variable can be access in that particular block only.
+   - Method scope or function scope: The initialize variable can be access accross function.
+ */
+public class index {
+
+    public static void main(String[] args) {
+      int p=5; //  here p is inside the function scope
+      {
+        int s=6; // s is inside the block scope
+        System.out.println(p);
+        System.out.println(s);
+    }  
+   // System.out.println(s); // her it will throw error as s is block scope variable
+    }
+}
