724174c8e668f94e4dd9a84fb3cc98f054191f80
sahil khan <sahilkhan2002786@gmail.com>
2024-06-15 09:49:47 +0530

    added total pair logic

--- 6_Arrays/9_SubArrays/index.java
+++ 6_Arrays/9_SubArrays/index.java
@@ -1,18 +1,21 @@
 
 public class index {
     public static void subArray(int number []){
+        int tp =0;
         for (int i = 0; i < number.length; i++) {
             for (int j = i; j < number.length; j++) {
                 for (int k = i; k <= j; k++) {
                     System.out.print(number[k]+ " ");
-                }
+                    }
+                tp++;
                 System.out.println();
             }
             System.out.println();
         }
+        System.out.print("Total no subArray pair is= "+ tp);
     }
     public static void main(String[] args) {
-        int number []= {2, 4, 6, 8, 10, 12};
+        int number []= {2, 4, 6, 8, 10, };
         subArray(number);
     }
 }
