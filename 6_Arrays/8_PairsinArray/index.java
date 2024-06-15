b47155ee5427823e951f8b4bcbf1068c53be8cbc
sahil khan <sahilkhan2002786@gmail.com>
2024-06-15 09:02:27 +0530

    Pairs in Array

---
+++ 6_Arrays/8_PairsinArray/index.java
@@ -0,0 +1,17 @@
+
+public class index {
+    public  static void pairsArray(int number[]){
+        for (int i = 0; i < number.length; i++) {
+            int curr = number[i];
+            for (int j = i+1; j < number.length; j++) {
+                System.out.print("( "+ curr + ", "+ number[j]+") ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int number []= {2, 4, 6, 8, 10};
+        pairsArray(number);
+    }
+    
+}
