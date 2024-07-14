ad32499b8ef115cc5918004ad49f13eaa63636ed
sahil khan <sahilkhan2002786@gmail.com>
2024-07-14 21:11:58 +0530

    Buy and sell stocks

---
+++ 6_Arrays/14_Buysellstock/index.java
@@ -0,0 +1,22 @@
+
+
+public class index {
+    public static int buySell (int prices []){
+        int buyPrices =Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            if(buyPrices < prices[i]){
+                int profit = prices[i] - buyPrices;
+                 maxProfit = Math.max(maxProfit, profit);
+            }else{
+                buyPrices = prices[i];
+            }
+        }
+        return  maxProfit;
+    }
+    public static void main(String[] args) {
+        int prices [] = {7, 1, 5, 6, 3, 4};
+        System.out.println(buySell(prices));
+    }
+}
