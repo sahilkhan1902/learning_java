25d480236a82cc6e6a6a06cd3007de292f779c66
Sahil Khan <sahilkhan@Nexgs-MacBook-Pro-4.local>
2025-01-05 17:04:23 +0530

    basic recursion till checking sorted aray

--- 9_Recursion/Basic/Index.java
+++ 9_Recursion/Basic/Index.java
@@ -1,4 +1,4 @@
-public class Index {
+public class index {
     public static void printDec(int n){
         if(n==1){
         System.out.println(n);
@@ -7,10 +7,54 @@ public class Index {
         System.out.println(n+" ");
         printDec(n-1);
     }
+    public static void printInc(int n){
+        if(n == 1){
+            System.out.println(n);
+            return;
+        }
+        printInc(n-1);
+        System.out.println(n+" ");
+        }
+    public static int printFact(int n){
+        if (n == 0){
+            return 1;
+        }
+       int fnm1 = printFact(n-1);
+       System.out.println();
+       int fnm2 = n* printFact(n-1);
+       return fnm2;
+    }
+    public static int calSum (int n){
+        if(n ==1 ){
+            return 1;
+        }
+        return n + calSum(n-1);
+    }
+    public static int printFibo (int n){
+        if (n ==0 ){
+            return 0;
+        }
+        if (n ==1 ){
+            return 1;
+        }
+        return  printFibo(n-1) + printFibo(n-2);
+    }
+    public static boolean isSorted(int arr [], int i){
+        if (arr[i] == arr.length-1){
+            return true;
+        }
+        if (arr[i]> arr[i+1]){
+            return false;
+        }
+         return isSorted(arr, i+1);
+    }
     public static void main (String[]args){
-     
-        int n =10;
-        printDec(n);
+     int arr[] = {1,6,4,5,6};
+        int n =5;
+        // printInc(n);
+        // printDec(n);
+        System.out.println(printFibo(n));
+        System.out.println(isSorted(arr, 0));
     }
 }
 
