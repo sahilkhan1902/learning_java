3988ce98640ef5d05110dc5550e47cc21866384c
Sahil Khan <sahilkhan2002786@gmail.com>
2025-01-10 20:41:08 +0530

    remove duplicate using string

--- 9_Recursion/intermediate/index.java
+++ 9_Recursion/intermediate/index.java
@@ -7,7 +7,23 @@ public class index {
         }
         return tilingProblen(n-1)+ tilingProblen(n-2);
     }
+    public static void removeDuplicates(String str, int idx, StringBuilder sb, boolean map[]){
+        if(idx == str.length()){
+            System.out.println(sb);
+        return;
+        }
+        char curr = str.charAt(idx);
+        if (map[curr-'a'] == true){
+            removeDuplicates(str, idx+1, sb, map);
+        } else{
+            map[curr-'a'] = true;
+            removeDuplicates(str, idx+1, sb.append(curr), map);
+        }
+    }
     public static void main(String[] args) {
+        String str = "aappnaacoollege";
+
+        removeDuplicates(str, 0, new StringBuilder("") , new boolean[26]);
       System.out.println( tilingProblen(3));
 
        
