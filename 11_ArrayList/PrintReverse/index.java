697812ab7ba45aa4071b28e6a8e772962ef99e2b
Sahil Khan <sahilkhan2002786@gmail.com>
2025-01-27 21:45:11 +0530

    print max number

---
+++ 11_ArrayList/PrintReverse/index.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+public class index { public static void main(String[] args) {
+    
+    ArrayList <Integer> list = new ArrayList<>();
+    list.add(1);
+    list.add(2);
+    list.add(3);
+    list.add(4);
+    list.add(5);
+ 
+
+    // reverse array List 0(n)
+    for (int index = list.size()-1; index >=0; index--) {
+        System.out.println(list.get(index)+ " ");
+        
+    }
+    int max = Integer.MIN_VALUE;
+    for (int index = 0; index < list.size(); index++) {
+        if(max < list.get(index)){
+            max = list.get(index);
+        }
+    }
+    System.out.println(max + " is the max number");
+
+
+}
+    
+}
